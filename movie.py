b538c12bdf90f73edce1333d85fbc418d25bb1ff
JulioLaz <julioalazarte@gmail.com>
2024-09-03 12:18:27 -0300

    corectionin the genres head(5-.num_movies)

--- movie.py
+++ movie.py
@@ -224,10 +224,10 @@ elif menu_id == "Login":
 else:
     ver_poster.title_poster_genre(menu_id,'Genre')
     df_genre = df_movies[df_movies['genres'] == menu_id].sort_values(by='rating', ascending=False)
-    lista_poster = list(df_genre['poster_path_full'].head(5))
-    lista_originalTitle = list(df_genre['title'].head(5))
-    lista_tconst = list(df_genre['imdb_id'].head(5))
-    lista_averageRating = list(round(df_genre['rating'],1).head(5))
+    lista_poster = list(df_genre['poster_path_full'].head(mun_movies))
+    lista_originalTitle = list(df_genre['title'].head(mun_movies))
+    lista_tconst = list(df_genre['imdb_id'].head(mun_movies))
+    lista_averageRating = list(round(df_genre['rating'],1).head(mun_movies))
     # vp.view_poster(lista_poster,lista_originalTitle,lista_tconst,lista_averageRating)
     ver_poster.view_poster(lista_poster,lista_originalTitle,lista_tconst,lista_averageRating)
 
